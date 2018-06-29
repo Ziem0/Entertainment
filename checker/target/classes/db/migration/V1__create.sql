ca4d7417cb7301ce803c8d38fa74f704d051c6ed
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-06-29 14:13:34 +0200

    remove target from checker module

--- checker/target/classes/db/migration/V1__create.sql
+++
@@ -1,1 +0,0 @@
-create table if not exists worker(id Integer primary key unique, name txt, age int);
--- checker/target/classes/db/migration/V2__insert.sql
+++
@@ -1,3 +0,0 @@
-insert into worker(name, age) values('ziemo',32);
-insert into worker(name, age) values('ania',30);
-
